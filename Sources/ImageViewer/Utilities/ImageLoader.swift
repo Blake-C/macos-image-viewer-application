5c0d83b592e6c21be4744c1a392be0050e9293ca
Blake Cerecero <blake@digitalblake.com>
2026-04-28 02:42:29 -0500

    Fixed thumbnail cache to key on (url, size) to prevent blurry masonry thumbnails

--- Sources/ImageViewer/Utilities/ImageLoader.swift
+++ Sources/ImageViewer/Utilities/ImageLoader.swift
@@ -4,16 +4,42 @@ import ImageIO
 
 // MARK: - Thumbnail cache
 
+private final class ThumbnailCacheKey: NSObject {
+    let url: URL
+    let size: CGFloat
+
+    init(url: URL, size: CGFloat) {
+        self.url  = url
+        self.size = size
+    }
+
+    override func isEqual(_ object: Any?) -> Bool {
+        guard let other = object as? ThumbnailCacheKey else { return false }
+        return url == other.url && size == other.size
+    }
+
+    override var hash: Int {
+        var h = Hasher()
+        h.combine(url)
+        h.combine(size)
+        return h.finalize()
+    }
+}
+
 private final class ThumbnailCache: @unchecked Sendable {
     static let shared = ThumbnailCache()
 
-    private let cache = NSCache<NSURL, NSImage>()
+    private let cache = NSCache<ThumbnailCacheKey, NSImage>()
 
     init() { cache.countLimit = 400 }
 
-    func get(_ url: URL) -> NSImage? { cache.object(forKey: url as NSURL) }
+    func get(_ url: URL, size: CGFloat) -> NSImage? {
+        cache.object(forKey: ThumbnailCacheKey(url: url, size: size))
+    }
 
-    func set(_ url: URL, image: NSImage) { cache.setObject(image, forKey: url as NSURL) }
+    func set(_ url: URL, size: CGFloat, image: NSImage) {
+        cache.setObject(image, forKey: ThumbnailCacheKey(url: url, size: size))
+    }
 
     func removeAll() { cache.removeAllObjects() }
 }
@@ -22,7 +48,7 @@ private final class ThumbnailCache: @unchecked Sendable {
 
 enum ImageLoader {
     static func thumbnail(for url: URL, size: CGFloat) async -> NSImage? {
-        if let cached = ThumbnailCache.shared.get(url) { return cached }
+        if let cached = ThumbnailCache.shared.get(url, size: size) { return cached }
         return await Task.detached(priority: .userInitiated) {
             guard let src = CGImageSourceCreateWithURL(url as CFURL, nil) else { return nil }
             let options: [CFString: Any] = [
@@ -33,7 +59,7 @@ enum ImageLoader {
             guard let cgImg = CGImageSourceCreateThumbnailAtIndex(src, 0, options as CFDictionary)
             else { return nil }
             let img = NSImage(cgImage: cgImg, size: NSSize(width: cgImg.width, height: cgImg.height))
-            ThumbnailCache.shared.set(url, image: img)
+            ThumbnailCache.shared.set(url, size: size, image: img)
             return img
         }.value
     }
